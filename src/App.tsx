2192f17f6b7559f481e2219d99cc84f214879348
Yash Chauhan <yashchauhan858@gmail.com>
2023-08-16 11:59:58 +0530

    Added react router dom and pages directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages";
+
 function App() {
-  return <div className=""></div>;
+  return (
+    <div className="">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
