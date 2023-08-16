fba14b7b165dfc018bdf7c1b22dfcfffff7a4826
Yash Chauhan <yashchauhan858@gmail.com>
2023-08-16 12:54:30 +0530

    Changed baseurl and added endpoint for up coming movies

--- src/network/endpoints.ts
+++ src/network/endpoints.ts
@@ -1,2 +1,3 @@
-export const tmdbBaseUrl =
-  "https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1";
+export const tmdbBaseUrl = "https://api.themoviedb.org/3/movie";
+
+export const upComingMovies = "/upcoming?language=en-US&page=1";
