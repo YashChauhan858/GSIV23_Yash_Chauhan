87cbee9da4c3429ee7282f2f5003ad8fe79afb5c
Yash Chauhan <yashchauhan858@gmail.com>
2023-08-17 16:17:55 +0530

    Added conditional component in header

--- src/components/Global/Header.tsx
+++ src/components/Global/Header.tsx
@@ -1,10 +1,16 @@
 import { AiFillHome } from "react-icons/ai";
 import { SearchComponent } from "..";
+import { useLocation } from "react-router-dom";
 
 const Header = () => {
+  const location = useLocation();
   return (
     <header className="p-2 flex justify-between items-center px-5 shadow-md shadow-[rgba(0,0,0,0.2)]">
-      <SearchComponent />
+      {location.pathname === "/details" ? (
+        <p className="text-lg text-gray-500 font-semibold">Movie Details</p>
+      ) : (
+        <SearchComponent />
+      )}
       <AiFillHome size={25} className="cursor-pointer" />
     </header>
   );
