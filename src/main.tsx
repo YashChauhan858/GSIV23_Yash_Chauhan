4bb454a04a5e166479af429223b1b613d2524aae
Yash Chauhan <yashchauhan858@gmail.com>
2023-08-17 15:47:50 +0530

    added redux provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,16 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Provider } from "react-redux";
+import { store } from "./store/store.ts";
 
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </QueryClientProvider>
 );
