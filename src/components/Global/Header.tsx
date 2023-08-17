efdee67748f279d45bc77e3164200e71aca8a305
Yash Chauhan <yashchauhan858@gmail.com>
2023-08-17 16:42:54 +0530

    Added position sticky to header

--- src/components/Global/Header.tsx
+++ src/components/Global/Header.tsx
@@ -5,7 +5,7 @@ import { useLocation } from "react-router-dom";
 const Header = () => {
   const location = useLocation();
   return (
-    <header className="p-2 flex justify-between items-center px-5 shadow-md shadow-[rgba(0,0,0,0.2)]">
+    <header className="p-2 flex justify-between items-center px-5 shadow-md shadow-[rgba(0,0,0,0.2)] sticky top-0 z-10 bg-white">
       {location.pathname === "/details" ? (
         <p className="text-lg text-gray-500 font-semibold">Movie Details</p>
       ) : (
