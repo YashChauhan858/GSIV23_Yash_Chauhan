e04c3c5be1fed734f81efa168ce253e4f8cb9ac9
Yash Chauhan <yashchauhan858@gmail.com>
2023-08-17 10:12:40 +0530

    Added react icons and css to search component

--- src/components/Home/SearchComponent.tsx
+++ src/components/Home/SearchComponent.tsx
@@ -1,7 +1,14 @@
+import { BiSearchAlt2 } from "react-icons/bi";
+
 const SearchComponent = () => {
   return (
-    <div>
-      <input type="text" />
+    <div className="flex items-center gap-3 bg-gray-500 p-2 w-fit rounded-sm">
+      <BiSearchAlt2 size={25} className="text-gray-400" />
+      <input
+        type="text"
+        className="bg-transparent outline-none placeholder:font-semibold text-white w-80"
+        placeholder="Search"
+      />
     </div>
   );
 };
